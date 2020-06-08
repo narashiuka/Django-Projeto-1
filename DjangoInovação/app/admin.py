648ba833e0846702907583cff5c65082688b274f
Gabriel Roberto Rodrigues Faria <gabriel.roberto.cep@gmail.com>
2020-06-08 00:54:21 -0300

    Criação da agenda no projeto

---
+++ DjangoInovação/app/admin.py
@@ -0,0 +1,10 @@
+
+
+from django.contrib import admin
+from app.models import Evento
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo','data_evento','data_criacao')
+    list_filter = ('Usuario',)
+
+admin.site.register(Evento, EventoAdmin)
--- DjangoInovação/app/models.py
+++ DjangoInovação/app/models.py
@@ -3,5 +3,18 @@ Definition of models.
 """
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    texto = models.TextField(blank=True, null=True)
+    Usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Evento'
+
+    def __str__(self):
+        return self.titulo
